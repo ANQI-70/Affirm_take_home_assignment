031bfab6cb8f880401f8ee98f826c2ee504eedf1
Anqi Chen <107441554+ANQI-70@users.noreply.github.com>
2024-10-14 09:31:52 -0400

    Update Q1 User_count.sql

--- Q1 User_count.sql
+++ Q1 User_count.sql
@@ -1,9 +1,9 @@
----Assumption: All fields are in right data type, for example:
-----Checkout_date: date
---- user_dob_year: integer
+-- Assumption: All fields are in right data type, for example:
+--  Checkout_date: date
+--  user_dob_year: integer
 
 SELECT 
-   Count(user_id) AS Count_users_with_loans
+   COUNT(user_id) AS Count_users_with_loans
 FROM loans --replace with the right dataset table name
 WHERE 
   DATE_FORMAT(checkout_date, '%Y-%m-%d') BETWEEN '2016-03-01' AND '2016-03-31'
